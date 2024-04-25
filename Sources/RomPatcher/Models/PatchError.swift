01d0e1aba4fe0b432102a02f55c04452329576ec
Mark Feaver <mark.feaver@gmail.com>
2024-04-25 20:44:17 +1000

    Began defining rudimentary RomPatcher protocol

---
+++ Sources/RomPatcher/Models/PatchError.swift
@@ -0,0 +1,13 @@
+//
+//  PatchError.swift
+//
+//
+//  Created by Mark Feaver on 25/4/2024.
+//
+
+public enum PatchError: Error {
+    // TODO: Add more cases once I know what I'm actually doing
+    case invalidROMData
+    case invalidPatchData
+    case unknown
+}
--- Sources/RomPatcher/RomPatcher.swift
+++ Sources/RomPatcher/RomPatcher.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public protocol RomPatcher {
+
+    func applyPatch(romData: Data, patchData: Data) throws -> Data
+
+}
